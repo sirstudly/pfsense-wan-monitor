9a7087d34efc17227461a2d45c03c90d3ebc52ff
sirstudly <ron1404@techie.com>
2025-08-29 01:45:26 +0100

    Add requirements installation for tapo_smart_plug.py in install script

--- install-wanmonitor.sh
+++ install-wanmonitor.sh
@@ -12,7 +12,12 @@ mkdir -p /usr/local/wanmonitor
 /usr/bin/fetch -o /usr/local/wanmonitor/wanmonitor.ini https://raw.githubusercontent.com/sirstudly/pfsense-wan-monitor/refs/heads/master/wanmonitor.ini
 /usr/bin/fetch -o /usr/local/wanmonitor/renew_dhcp.sh https://raw.githubusercontent.com/sirstudly/pfsense-wan-monitor/refs/heads/master/renew_dhcp.sh
 /usr/bin/fetch -o /usr/local/wanmonitor/tapo_smart_plug.py https://raw.githubusercontent.com/sirstudly/pfsense-wan-monitor/refs/heads/master/tapo_smart_plug.py
+/usr/bin/fetch -o /usr/local/wanmonitor/requirements.txt https://raw.githubusercontent.com/sirstudly/pfsense-wan-monitor/refs/heads/master/requirements.txt
 chmod +x /usr/local/wanmonitor/renew_dhcp.sh
+
+# The following is only required if using tapo_smart_plug.py
+/usr/local/bin/python3.11 -m ensurepip
+/usr/local/bin/pip3 install --no-cache-dir -r /usr/local/wanmonitor/requirements.txt
 echo "Configure /usr/local/wanmonitor/wanmonitor.ini manually."
 
 # Add the startup variable to rc.conf.local.
